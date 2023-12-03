0e752d25c636cb8cf5219e12d3310886c50b814b
Denis Shahbazyan <den.shahbazyan@yandex.ru>
2023-12-03 17:21:39 +0300

    Валидация и документирование параметров запросов

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, Query
 from typing import Optional
 from enum import Enum
 
@@ -24,23 +24,28 @@ def hello_author() -> dict[str, str]:
     response_description='Полная строка приветствия',
 )
 def greetings(
-    *,
-    surname: str,
-    age: Optional[int] = None,
-    is_staff: bool = False,
-    education_level: Optional[EducationLevel] = None,
-    name: str,
+        *,
+        name: str = Path(
+            ..., min_length=2, max_length=20,
+            title='Полное имя', description='Можно вводить в любом регистре'
+        ),
+        surname: list[str] = Query(..., min_length=2, max_length=50),
+        age: Optional[int] = Query(None, gt=4, le=99),
+        is_staff: bool = Query(
+            False, alias='is-staff', include_in_schema=False
+        ),
+        education_level: Optional[EducationLevel] = None,
 ) -> dict[str, str]:
     """
     Приветствие пользователя:
 
     - **name**: имя
-    - **surname**: фамилия
+    - **surname**: фамилия или несколько фамилий
     - **age**: возраст (опционально)
-    - **is_staff**: является ли пользователь сотрудником
-    - **education_level**: уровень образования (опционально)
+    - **title**: обращение
     """
-    result = ' '.join([name, surname])
+    surnames = ' '.join(surname)
+    result = ' '.join([name, surnames])
     result = result.title()
     if age is not None:
         result += ', ' + str(age)
